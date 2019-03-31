fbff85e5af1fcb96d68ab772b1b03a3a40386ccc
Kunzhen Wang <wangkz@garena.com>
2019-03-31 22:48:48 +0800

    Fix conflicting symbols

--- TradeOptimizer.py
+++ TradeOptimizer.py
@@ -10,13 +10,18 @@ import sys
 import csv
 import pandas as pd
 import numpy as np
-from os import listdir
-from os.path import isfile, join
+from shutil import rmtree
+from os import listdir, mkdir
+from os.path import isfile, join, exists
 from OptimizationEngine import OptimizationEngine
 from ExecutionEngine import ExecutionEngine
 
 data_folder = sys.argv[1]
-output_folder = data_folder + "/output/"
+output_folder = data_folder + "/output"
+
+if exists(output_folder):
+    rmtree(output_folder)
+mkdir(output_folder)
 
 volume = int(sys.argv[2])
 volume_step = int(sys.argv[3])
@@ -54,16 +59,17 @@ for (msg_book_file, order_book_file) in zip(msg_book_files, order_book_files):
                                              volume_step,
                                              int(volume / volume_step),
                                              actions)
-        start_time = time.time()
+        start = time.time()
         execution_engine = ExecutionEngine()
         strategy = optimize_engine.compute_optimal_solution(volume, execution_engine)
-        elapse_time = time.time() - start_time
+        elapse_time = time.time() - start
         print("Done execution for " + str(start_time) + " using " + str(elapse_time))
         print(strategy)
         daily_result_entry = [item for tup in zip(strategy.actions, strategy.inventory) for item in [tup[0], tup[1]]] + [strategy.cost]
         daily_result.append(daily_result_entry)
 
-    output_filename = "output.csv"
-    with open(join(data_folder, output_filename), "w") as f:
+    date_string = msg_book_file.split("_")[1]
+    output_filename = "optimal_solution_" + date_string + ".csv"
+    with open(join(output_folder, output_filename), "w") as f:
         writer = csv.writer(f)
         writer.writerows(daily_result)
