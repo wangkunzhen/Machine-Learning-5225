32fa9c15ee77293c6c03ecb5125d05f25e8dce7a
Kunzhen Wang <wangkz@garena.com>
2019-03-31 15:43:18 +0800

    Fix optimisation engine

--- OptimizationEngine.py
+++ OptimizationEngine.py
@@ -1,65 +1,72 @@
 import numpy as np
-from all_cost import ExecutionEngine
 from math import floor
-from functools import  reduce
+from functools import reduce
 
 
-class Result:
+class Strategy:
     def __init__(self, cost, actions):
         self.cost = cost
         self.actions = actions
 
 
 class OptimizationEngine:
-    def __init__(self, order_book, message_book, possible_inventories):
+    """
+    Given order book & message book entries for an episode, calculate the optimal strategy.
+    Output is a Strategy object with an array of actions and its associated cost.
+    """
+
+    def __init__(self, order_book, message_book, time_step, time_count, inventory_step, inventory_count, actions):
         self.order_book = order_book
         self.message_book = message_book
-        self.possible_inventories = possible_inventories
-
-    def order_book_entries(self, time):
-        return []
-
-    def message_book_entries(self, time):
-        return []
+        self.time_step = time_step
+        self.time_count = time_count
+        self.inventory_step = inventory_step
+        self.inventory_count = inventory_count
+        self.actions = actions
 
     @staticmethod
     def mid_spread_from_order_book(order_book):
         return ((order_book[:, 0] + order_book[:, 2]) / 2)[0]
 
     @staticmethod
-    def cost_function(actions, costs, inventories, next_results, unit_inventory):
+    def cost_update(actions, costs, inventories, next_results, unit_inventory):
         next_period_results = [next_results[floor(i / unit_inventory)] for i in inventories]
         tuples = zip(actions, costs, next_period_results)
-        total_results = [Result(c + r.cost, [a] + r.actions) for (a, c, r) in tuples]
+        total_results = [Strategy(c + r.cost, [a] + r.actions) for (a, c, r) in tuples]
         return reduce(lambda x, y: x if x.cost == min(x.cost, y.cost) else y, total_results)
 
-    def calculate_optimal_solution(self, actions, periods_count, total_inventory):
-        inventories_count = len(self.possible_inventories)
-        results = np.array(inventories_count)
+    def order_book_entries(self, time_index):
+        lower = time_index * self.time_step
+        upper = (time_index + 1) * self.time_step
+        filtered_tuples = filter(lambda o, m: lower <= m < upper, zip(self.message_book, self.order_book))
+        return [o for (o, m) in filtered_tuples]
 
+    def message_book_entries(self, time_index):
+        lower = time_index * self.time_step
+        upper = (time_index + 1) * self.time_step
+        return [lower <= m[0] < upper for m in self.message_book]
+
+    def compute_optimal_solution(self, total_inventory, execution_engine):
         # Initialize for time T
-        for idx in range(0, inventories_count):
-            order_book = self.order_book_entries(periods_count)
-            mid_spread = OptimizationEngine.mid_spread_from_order_book(order_book)
-            inventory = self.possible_inventories[idx]
-            cost = ExecutionEngine.cost_T(order_book, mid_spread, inventory)
-            results[idx] = Result(cost, [])
+        last_order_book = self.order_book_entries(self.time_count)
+        last_mid_spread = OptimizationEngine.mid_spread_from_order_book(last_order_book)
+        results = [Strategy(execution_engine.cost_T(last_order_book, last_mid_spread, idx * self.inventory_step), [])
+                   for idx in range(0, self.inventory_count + 1)]
 
         # Back Propagation
-        for t in range(periods_count-1, 0, -1):
+        for t in range(self.time_count-1, -1, -1):
             order_book = self.order_book_entries(t)
             message_book = self.message_book_entries(t)
             mid_spread = OptimizationEngine.mid_spread_from_order_book(order_book)
-            curr_results = np.array(inventories_count)
 
-            for idx in range(0, inventories_count):
-                inventory = self.possible_inventories[idx]
-                costs, remaining_inventories = ExecutionEngine.cost_other(message_book,
-                                                                          order_book,
-                                                                          inventory,
-                                                                          mid_spread,
-                                                                          actions)
-                curr_results[idx] = self.cost_function(costs, remaining_inventories, results)
+            curr_results = np.array(self.inventory_count + 1)
+            for idx in range(0, self.inventory_count + 1):
+                costs, remaining_inventories = execution_engine.cost_other(message_book,
+                                                                           order_book,
+                                                                           idx * self.inventory_step,
+                                                                           mid_spread,
+                                                                           self.actions)
+                curr_results[idx] = self.cost_update(costs, remaining_inventories, results)
 
             results = curr_results
 
