54f09f4763fab8db40c361ed961c72a7a8e932df
Kunzhen Wang <wangkz@garena.com>
2019-04-09 00:42:50 +0800

    Add model training

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from os.path import join, exists
 from os import mkdir
 from Market import Market
 from TradeOptimizer import TradeOptimizer
+from Model import Model
 
 # arguments
 data_folder = sys.argv[1]
@@ -26,6 +27,7 @@ train_private_output = join(private_folder, "Train")
 test_data_folder = join(data_folder, "Test")
 test_market_output = join(market_folder, "Test")
 test_private_output = join(private_folder, "Test")
+model_output = join(output_folder, "Model")
 
 if not exists(output_folder):
     mkdir(output_folder)
@@ -36,8 +38,12 @@ if not exists(market_folder):
 if not exists(private_folder):
     mkdir(private_folder)
 
+if not exists(model_output):
+    mkdir(model_output)
+
 trade_start = int(9.5*60*60)
 trade_end = int(16*60*60)
+window_size = 1
 
 # compute market variable
 Market(train_data_folder, train_market_output, time_horizon, time_step, moving_window, trade_start, trade_end).load()
@@ -47,3 +53,17 @@ Market(test_data_folder, test_market_output, time_horizon, time_step, moving_win
 train_opt = TradeOptimizer(train_data_folder, train_private_output, volume, volume_step, time_horizon, time_step,
                            max_action, min_action, -action_step, trade_start, trade_end)
 train_opt.optimize_trade_execution()
+
+private_opt = TradeOptimizer(test_data_folder, train_private_output, volume, volume_step, time_horizon, time_step,
+                             max_action, min_action, -action_step, trade_start, trade_end)
+private_opt.optimize_trade_execution()
+
+# fit model
+model_trainer = Model(private_folder, market_folder, volume, volume_step, time_horizon, time_step,
+                      max_action, min_action, action_step, window_size)
+model, loss, accuracy = model_trainer.fit_model(10)
+model_json = model.to_json()
+with open(join(model_output, "model.json"), "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model_weights.h5")
+
